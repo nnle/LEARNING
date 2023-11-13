7cfc72a628adb4df88f9af70992bd319dfa61efa
nnle <ngngle@gmail.com>
2023-11-13 11:05:21 +0700

    date 13/11/2023

---
+++ NEW TECHNOLOGY/REAL ESTATE PREDICTION/app.py
@@ -0,0 +1,28 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model/model.pkl', 'rb'))
+
+@app.route('/')   
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    #return render_template('index.html', prediction_text='Expense = {}'.format(output))
+    return render_template('index.html', prediction_result='Price Pediction = {}'.format(output))
+
+
+if __name__ == "__main__":
+    #app.run(port=8080)
+    app.run(host='0.0.0.0',port=8080)
+    #app.run(debug=True)
